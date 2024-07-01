3ad9a6b97f7a68f3de393e5d31261149a0f32e0e
Mahran_AD <mhranabwdqt971@gmail.com>
2024-07-01 20:41:10 +0300

    sync the sound

--- sound.js
+++ sound.js
@@ -5,23 +5,26 @@ class SoundPlayer {
     this.listener = new THREE.AudioListener();
     this.audioLoader = new THREE.AudioLoader();
     this.audio = new THREE.Audio(this.listener);
+    this.secondAudio = null; // Store the second audio instance
     this.isSoundPlaying = false;
+    this.volume = 0.2;
+    this.volumeChangeRate = 0.2; // smaller increments
+    this.volumeChangeInterval = 300; // shorter interval for smoother changes
+    this.increaseVolumeInterval = null;
+    this.decreaseVolumeInterval = null;
   }
 
   loadSound(audioFilePath) {
     this.audioLoader.load(audioFilePath, (buffer) => {
       this.audio.setBuffer(buffer);
       this.audio.setLoop(false);
-      this.audio.setVolume(0.5);
+      this.audio.setVolume(this.volume); // Use the initial volume
 
-      document.addEventListener('keydown', (event) => {
-        if (event.key === 't' || event.key === 'T') {
-          if (!this.isSoundPlaying) {
-            this.playSound();
-          }
-        }
-      });
-      
+      console.log("Sound loaded with initial volume:", this.volume);
+
+      document.addEventListener('keydown', this.handleKeyDown.bind(this));
+      document.addEventListener('keyup', this.handleKeyUp.bind(this));
+
       this.audio.onEnded = () => {
         this.isSoundPlaying = false;
         this.loadSecondSound();
@@ -29,22 +32,87 @@ class SoundPlayer {
     });
   }
 
+  handleKeyDown(event) {
+    if (event.key === 't' || event.key === 'T') {
+      if (!this.isSoundPlaying) {
+        this.playSound();
+      }
+    } else if (event.key === 'ArrowUp') {
+      console.log("ArrowUp pressed - Starting to increase volume");
+      this.startIncreasingVolume();
+    }
+  }
+
+  handleKeyUp(event) {
+    if (event.key === 'ArrowUp') {
+      console.log("ArrowUp released - Starting to decrease volume");
+      this.stopIncreasingVolume();
+      this.startDecreasingVolume();
+    }
+  }
+
   loadSecondSound() {
-    //let con=window.congear;
     const secondAudioFilePath = 'sound/rest.mp3';
     this.audioLoader.load(secondAudioFilePath, (buffer) => {
-      const secondAudio = new THREE.Audio(this.listener);
-      secondAudio.setBuffer(buffer);
-      secondAudio.setLoop(true);
-      secondAudio.setVolume(0.5);
-      secondAudio.play();
+      this.secondAudio = new THREE.Audio(this.listener);
+      this.secondAudio.setBuffer(buffer);
+      this.secondAudio.setLoop(true);
+      this.secondAudio.setVolume(this.volume); // Set initial volume
+      this.secondAudio.play();
       this.isSoundPlaying = true;
     });
   }
 
   playSound() {
+    console.log("Playing sound");
     this.audio.play();
     this.isSoundPlaying = true;
+  }
+
+  startIncreasingVolume() {
+    this.clearVolumeIntervals();
+    this.increaseVolumeInterval = setInterval(() => {
+      if (this.volume < 1.0) {
+        this.volume = Math.min(1.0, this.volume + this.volumeChangeRate);
+        console.log("Increasing volume:", this.volume);
+        this.audio.setVolume(this.volume);
+        if (this.secondAudio) {
+          this.secondAudio.setVolume(this.volume);
+        }
+      }
+    }, this.volumeChangeInterval);
+  }
+
+  stopIncreasingVolume() {
+    if (this.increaseVolumeInterval) {
+      clearInterval(this.increaseVolumeInterval);
+      this.increaseVolumeInterval = null;
+    }
+  }
+
+  startDecreasingVolume() {
+    this.clearVolumeIntervals();
+    this.decreaseVolumeInterval = setInterval(() => {
+      if (this.volume > 0.0) {
+        this.volume = Math.max(0.0, this.volume - this.volumeChangeRate);
+        console.log("Decreasing volume:", this.volume);
+        this.audio.setVolume(this.volume);
+        if (this.secondAudio&&this.volume >0.2) {
+          this.secondAudio.setVolume(this.volume);
+        }
+      }
+    }, this.volumeChangeInterval);
+  }
+
+  clearVolumeIntervals() {
+    if (this.increaseVolumeInterval) {
+      clearInterval(this.increaseVolumeInterval);
+      this.increaseVolumeInterval = null;
+    }
+    if (this.decreaseVolumeInterval) {
+      clearInterval(this.decreaseVolumeInterval);
+      this.decreaseVolumeInterval = null;
+    }
   }
 }
 
